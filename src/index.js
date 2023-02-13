7c698da666cb337fd6c7c794587adb1e1370aa58
timeeatsflesh <Robert@Roberts-MacBook.local>
2023-02-13 12:50:59 -0700

    second push

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState } from "react";
 const Color = (props) => {
-  return (
-    <div className={props.color} />
-  )
-}
-
+  return( <div className={props.color} onClick={() => props.selectColor(props.color)} />
+)};
 
 const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color='yellow'/>
-        <Color color='green'/>
-        <Color color='violet'/>
+        <Color color="yellow" selectColor={selectColor} />
+        <Color color="green" selectColor={selectColor} />
+        <Color color="violet" selectColor={selectColor} />
       </div>
     </div>
   );
-}
+};
 
-ReactDOM.render(
-  <Picker />,
-  document.getElementById('app')
-);
+const selectColor = (colorName) => {
+  setSelectedColor(colorName);
+};
+
+ReactDOM.render(<Picker />, document.getElementById("app"));
